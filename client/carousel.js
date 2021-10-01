3520d28303e27c3c2f67060ddc5f0cb409015758
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-01 00:01:51 -0400

    Added functionality for carousel with file system

--- client/carousel.js
+++ client/carousel.js
@@ -1,19 +1,29 @@
+const carouselImgsDir = '../res/landing-carousel/carousel';
+
 const carouselButtons = document.querySelectorAll('.img-btn');
 const mainImg = document.querySelector('#sample-main');
-const sideImgs = document.querySelectorAll('.sample-side')
+const sideImgs = document.querySelectorAll('.sample-side');
 
-console.log(carouselButtons[0]);
+let curMain = 2;
 
 carouselButtons[0].addEventListener('click', () => {
-    let tempSrc = sideImgs[0].src;
+    curMain++;
+    if (curMain > 5) {
+        curMain = 1;
+    }
+
     sideImgs[0].src = mainImg.src;
     mainImg.src = sideImgs[1].src;
-    sideImgs[1].src = tempSrc;
+    sideImgs[1].src = carouselImgsDir + ((curMain === 5) ? 1: curMain + 1) + '.jpg';
 });
 
 carouselButtons[1].addEventListener('click', () => {
-    let tempSrc = sideImgs[1].src;
+    curMain--;
+    if (curMain < 1) {
+        curMain = 5;
+    }
+
     sideImgs[1].src = mainImg.src;
     mainImg.src = sideImgs[0].src;
-    sideImgs[0].src = tempSrc;
+    sideImgs[0].src = carouselImgsDir + ((curMain === 5) ? 1: curMain + 1) + '.jpg';
 });
