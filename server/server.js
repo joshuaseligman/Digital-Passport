6fbd069e7faa346d31f27722735b5d8082e453cd
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-01 15:42:05 -0400

    Added routes to the rest of the pages

--- server/server.js
+++ server/server.js
@@ -13,6 +13,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(projectDir, 'pages/index.html'));
 });
 
+app.get('/map', (req, res) => {
+    res.sendFile(path.join(projectDir, 'pages/map.html'));
+});
+
+app.get('/collection', (req, res) => {
+    res.sendFile(path.join(projectDir, 'pages/collection.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(projectDir, 'pages/login.html'));
+});
+
+app.get('/posts', (req, res) => {
+    res.sendFile(path.join(projectDir, 'pages/posts.html'));
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
