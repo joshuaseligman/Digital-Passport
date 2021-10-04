5e72da0055fb68a2fd4656f23f118534da2527a9
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-04 19:52:56 -0400

    Got neaby locations to where the user clicks on the map

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,9 @@
 const map = L.map('map').setView([25, 0], 2);
 let marker = undefined;
 
+const info = document.querySelectorAll('.info');
+const nearbyLocations = [];
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{username}/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -22,9 +25,33 @@ map.on('click', (e) => {
     fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${e.latlng['lat']}&longitude=${e.latlng['lng']}&localityLanguage=en`)
     .then(response => response.json())
     .then(data => {
-        const info = document.querySelectorAll('.info');
         info[0].textContent = data['city'] !== '' ? data['city'] : data['locality'];
         info[1].textContent = data['principalSubdivision'];
         info[2].textContent = data['countryCode'];
+
+        getNearbyCities(e.latlng);
     });
 });
+
+function getNearbyCities(latLong) {
+    for (let i = nearbyLocations.length - 1; i >= 0; i--) {
+        nearbyLocations.pop();
+    }
+
+    const fiveMilesInDeg = 10 / 69;
+    
+    for (let lat = latLong['lat'] - fiveMilesInDeg; lat < latLong['lat'] + fiveMilesInDeg; lat += 0.03) {
+        for (let long = latLong['lng'] - fiveMilesInDeg; long < latLong['lng'] + fiveMilesInDeg; long += 0.03) {
+            fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
+            .then(response => response.json())
+            .then(data => {
+                let location = data['city'] !== '' ? data['city'] : data['locality'];
+                if (location !== info[0].textContent) {
+                    if (nearbyLocations.indexOf(location) === -1) {
+                        nearbyLocations.push(location);
+                    }
+                }
+            });
+        }
+    }
+}
