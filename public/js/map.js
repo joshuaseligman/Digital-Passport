b691941f8c6c0690e4f1e3e12f2815a7e91b50a4
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-04 18:59:10 -0400

    Obtained basic information based on marker location on map

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,11 @@
 const map = L.map('map').setView([25, 0], 2);
 let marker = undefined;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{username}/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    username: 'joshyankfan01',
+    id: 'ckud4bw6xa0hs17mktixatxuq',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoiam9zaHlhbmtmYW4wMSIsImEiOiJja3U4bXh4dmEyZmV4Mm5uaTEzNjk2c2w5In0.Mk0ce1C_XTpg1scZ3wWTGg'
@@ -17,4 +18,13 @@ map.on('click', (e) => {
         marker.setLatLng(e.latlng);
     }
     map.setView(e.latlng, 12);
+
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${e.latlng['lat']}&longitude=${e.latlng['lng']}&localityLanguage=en`)
+    .then(response => response.json())
+    .then(data => {
+        const info = document.querySelectorAll('.info');
+        info[0].textContent = data['city'] !== '' ? data['city'] : data['locality'];
+        info[1].textContent = data['principalSubdivision'];
+        info[2].textContent = data['countryCode'];
+    });
 });
