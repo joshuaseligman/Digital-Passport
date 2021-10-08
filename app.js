4af71de1156ca34e715b88f4414969ee01aab16b
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-07 21:44:27 -0400

    Continued working on the posts selection process. Having styling issues now

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.port || 3000;
+const port = 3000;
+
+const posts = [
+    {id: 1, src: '/res/posts-imgs/post1.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 2, src: '/res/posts-imgs/post2.jpg', city: 'Princeton', state: 'New Jersey', country: 'USA', user: 'joshseligman', date: {month: 'May', day: 4, year: 1976}},
+    {id: 3, src: '/res/posts-imgs/post3.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 4, src: '/res/posts-imgs/post4.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 5, src: '/res/posts-imgs/post5.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 6, src: '/res/posts-imgs/post6.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 7, src: '/res/posts-imgs/post7.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 8, src: '/res/posts-imgs/post8.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+    {id: 9, src: '/res/posts-imgs/post9.jpg', city: 'London', state: 'England', country: 'GBR', user: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
+];
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -30,8 +42,21 @@ app.get('/posts', (req, res) => {
 });
 
 app.get('/postSelection', (req, res) => {
-    res.render('postSelection');
+    let curPostIndex = req.query.cur;
+    let cPost = getCurPost(curPostIndex);
+    res.render('postSelection', {posts : posts, curPost: cPost});
 });
+
+function getCurPost(postIndex) {
+    const index = posts.findIndex((post) => {
+        return post.id === parseInt(postIndex);
+    });
+    if (index < 0) {
+        return {id: -1, error: 'No such post exists'};
+    } else {
+        return posts[index];
+    }
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- public/js/postSelection.js
+++ public/js/postSelection.js
@@ -1,20 +1,5 @@
 const postArea = document.querySelector('#posts-area');
 
-for (let i = 1; i <= 9; i++) {
-    const post = document.createElement('img');
-    post.src = `/res/posts-imgs/post${i}.jpg`;
-    post.classList.add('post-link');
-
-    postArea.appendChild(post);
-}
-
-const posts = document.querySelectorAll('.post-link');
-let visible = false;
-
-for (let j = 0; j < posts.length; j++) {
-    posts[j].addEventListener('click', toggleOverlapPostArea);
-}
-
 const overlapPostArea = document.querySelector('#overlap-post-area');
 overlapPostArea.style.height = postArea.offsetHeight + 'px';
 overlapPostArea.style.marginTop = '-' + overlapPostArea.style.height;
@@ -22,12 +7,9 @@ overlapPostArea.style.width = postArea.offsetWidth + 'px';
 overlapPostArea.addEventListener('click', toggleOverlapPostArea);
 
 function toggleOverlapPostArea(event) {
-    if (visible) {
-        if (event.target.id == 'overlap-post-area') {
-            overlapPostArea.style.display = 'none';
-        }
-    } else {
-        overlapPostArea.style.display = 'flex';
+    if (event.target.id == 'overlap-post-area') {
+        window.location.replace(`/postSelection`);
     }
-    visible = !visible;
 }
+
+
