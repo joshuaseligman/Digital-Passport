e0a6fbe32961354a79e5d84c68802f790d1ee868
Josh Seligman <joshua.seligman1@marist.edu>
2021-10-10 14:52:54 -0400

    Added log in, log out, and sign up functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const url = require('url');
 
 const app = express();
 
@@ -21,8 +22,14 @@ const posts = [
     {id: 9, src: '/res/posts-imgs/post9.jpg', caption: 'This is a great caption', city: 'London', state: 'England', country: 'GBR', username: 'joshseligman', date: {month: 'June', day: 1, year: 2021}},
 ];
 
+const accounts = [
+    {id: 1, username: "joshseligman", password: "helloworld"}
+]
+
+let curAcct = {id: 0}
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {account: curAcct});
 });
 
 app.get('/map', (req, res) => {
@@ -33,7 +40,7 @@ app.get('/map', (req, res) => {
     if (city !== undefined && country !== undefined) {
         res.redirect(`/postSelection?city=${city}&state=${state}&country=${country}`.replace(' ', '%20'));
     } else {
-        res.render('map');
+        res.render('map', {account: curAcct});
     }
 });
 
@@ -42,7 +49,37 @@ app.get('/collection', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    const signup = req.query.signup;
+    if (signup === 'true') {
+        let uname = req.query.username;
+        const index = accounts.findIndex((acct) => {
+            return acct.username === uname;
+        });
+        if (index === -1) {
+            curAcct = {id: Math.floor(Math.random() * 10000) + 1,username: uname, password: req.query.password};
+            accounts.push(curAcct);
+        } else {
+            curAcct = {id: -1, error: "signup"};
+        }
+    } else if (signup === 'false') {
+        const index = accounts.findIndex((acct) => {
+            return acct.username === req.query.username
+            && acct.password === req.query.password;
+        });
+        if (index === -1) {
+            curAcct = {id: -1, error: "login"};
+        } else {
+            curAcct = accounts[index];
+        }
+    }
+
+    if (curAcct.id === 0) {
+        res.render('login', {account: curAcct});
+    } else if (curAcct.id === -1) {
+        res.render('login', {account: curAcct})
+    } else {
+        res.redirect('/');
+    }
 });
 
 app.get('/posts', (req, res) => {
@@ -58,8 +95,17 @@ app.get('/postSelection', (req, res) => {
 
     const curPostIndex = req.query.cur;
     const cPost = getCurPost(curPostIndex, goodPosts);
-    res.render('postSelection', {posts : goodPosts, curPost: cPost});
+    res.render('postSelection', {account: curAcct, posts : goodPosts, curPost: cPost});
 });
+
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
+
+app.get('/logout', (req, res) => {
+    curAcct = {id: 0};
+    res.redirect('/');
+})
 
 function getCurPost(postIndex, postsToSearch) {
     const index = postsToSearch.findIndex((post) => {
